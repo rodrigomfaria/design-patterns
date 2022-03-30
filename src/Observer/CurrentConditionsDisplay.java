a9cfa8497755fc1fc802575efff897c82775388b
Rodrigo Faria <rodrigomfaria@gmail.com>
2022-03-30 15:42:37 -0300

    Observer

---
+++ src/Observer/CurrentConditionsDisplay.java
@@ -0,0 +1,26 @@
+package Observer;
+
+public class CurrentConditionsDisplay implements Observer, DisplayElement {
+
+	private float temperature;
+	private float humidity;
+
+	public CurrentConditionsDisplay(Subject weatherData) {
+		weatherData.registerObserver(this);
+	}
+
+	@Override
+	public void update(float temperature, float humidity, float pressure) {
+		this.temperature = temperature;
+		this.humidity = humidity;
+		display();
+
+	}
+
+	@Override
+	public void display() {
+		System.out.println("Current conditions: " + temperature + "F degree and " + humidity + "% humidity");
+
+	}
+
+}
---
+++ src/Observer/DisplayElement.java
@@ -0,0 +1,7 @@
+package Observer;
+
+public interface DisplayElement {
+
+	public void display();
+
+}

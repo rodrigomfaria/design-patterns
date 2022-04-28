1dfb9d351566f8c57c476c3d8fcb87f97525cfbd
Rodrigo Faria <rodrigomfaria@gmail.com>
2022-04-28 12:13:51 -0300

    Singleton Pattern

---
+++ src/Singleton/Singleton.java
@@ -0,0 +1,21 @@
+package Singleton;
+
+public class Singleton {
+
+	private static Singleton uniqueInstance;
+
+	private Singleton() {
+	}
+
+	public static Singleton getInstance() {
+		if (uniqueInstance == null) {
+			uniqueInstance = new Singleton();
+		}
+		return uniqueInstance;
+	}
+
+	public String getDescription() {
+		return "I'm a classic Singleton!";
+	}
+
+}
---
+++ src/Singleton/SingletonClient.java
@@ -0,0 +1,10 @@
+package Singleton;
+
+public class SingletonClient {
+
+	public static void main(String[] args) {
+		Singleton singleton = Singleton.getInstance();
+		System.out.println(singleton.getDescription());
+	}
+
+}
